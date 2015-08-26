3bcb0ddcfb62d9faf4aef662cccf05b4882ed3fb
Simon Welker <simon.welker@esirion.de>
2015-08-26 17:04:11 +0200

    p 395; only tests

---
+++ src/loader.js
@@ -0,0 +1,16 @@
+/* jshint globalstrict: true */
+'use strict';
+
+function setupModuleLoader(window) {
+  var ensure = function(obj, name, factory) {
+    return obj[name] || (obj[name] = factory());
+  };
+
+  var angular = ensure(window, 'angular', Object);
+
+  ensure(angular, 'module', function() {
+    return function() {
+
+    };
+  });
+}
